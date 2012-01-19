dd75886cb35a3fb2e67926888c8c2daf4bcf8a71
Baptiste Lafontaine <baptiste33@gmail.com>
2012-01-19 16:30:09 +0100

    Update

--- js/references.js
+++ js/references.js
@@ -1,8 +1,8 @@
 function generateReference() {
-	$("aside").each(function(i) {
+	$("span.ref").each(function(i) {
 		var o = i+1;
 		var current = $(this);
 		$("#listRef").append("<p id='ref" + o + "'>[" + o + "] " + current.html() + "</p>");
-		current.html(" <a class='ref' href='#ref" + o +"'>#[" + o +"]</a> ");
+		current.html(" <a class='ref' href='#ref" + o +"'>[" + o +"]</a> ");
 	});
 }
